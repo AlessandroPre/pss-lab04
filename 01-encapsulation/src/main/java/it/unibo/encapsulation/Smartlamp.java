e8e4b09990e06d0759d04d8d46ee801b570f35c9
Alessandro Pretolani <alessandro.pretolan6@studio.unibo.it>
2023-10-17 10:40:01 +0200

    Smartlamp.java creato

---
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/Smartlamp.java
@@ -0,0 +1,18 @@
+package it.unibo.encapsulation;
+
+public class Smartlamp{
+    
+    private boolean LampOn;
+
+    private static int[] INTENSITY = {1,2,3};
+    private static String[] COLOR = {"white","red","green", "blue"};
+
+    public Smartlamp(){
+        this.LampOn = false;
+    }
+
+    public void SwitchOn(){
+
+    }
+
+}
